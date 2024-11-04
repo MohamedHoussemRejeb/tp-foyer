404ab7e575569734169dde3ead1eb662389845b6
MohamedHoussemRejeb <mohamedhoussem.rejeb@esprit.tn>
2024-11-04 17:28:45 +0100

    Ajout de la journalisation dans FoyerServiceImpl

--- src/main/java/tn/esprit/tpfoyer/service/FoyerServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/FoyerServiceImpl.java
@@ -1,7 +1,8 @@
 package tn.esprit.tpfoyer.service;
 
-
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import tn.esprit.tpfoyer.entity.Foyer;
 import tn.esprit.tpfoyer.repository.FoyerRepository;
@@ -12,21 +13,31 @@ import java.util.List;
 @AllArgsConstructor
 public class FoyerServiceImpl implements IFoyerService {
 
-    FoyerRepository foyerRepository;
+    private static final Logger logger = LoggerFactory.getLogger(FoyerServiceImpl.class);
+    private final FoyerRepository foyerRepository;
+
     public List<Foyer> retrieveAllFoyers() {
+        logger.info("Récupération de tous les foyers");
         return foyerRepository.findAll();
     }
+
     public Foyer retrieveFoyer(Long foyerId) {
-        return foyerRepository.findById(foyerId).get();
+        logger.info("Récupération du foyer avec ID: {}", foyerId);
+        return foyerRepository.findById(foyerId).orElse(null);
     }
+
     public Foyer addFoyer(Foyer f) {
+        logger.info("Ajout d'un nouveau foyer: {}", f);
         return foyerRepository.save(f);
     }
+
     public Foyer modifyFoyer(Foyer foyer) {
+        logger.info("Modification du foyer: {}", foyer);
         return foyerRepository.save(foyer);
     }
 
     public void removeFoyer(Long foyerId) {
+        logger.info("Suppression du foyer avec ID: {}", foyerId);
         foyerRepository.deleteById(foyerId);
     }
 }
